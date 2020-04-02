95310f8adffa3bb3395bdafddaa5c49df4112114
Zorro Liu <lyx@rock-chips.com>
2020-04-02 16:52:01 +0800

    gResetEpdMode no need to set when property enable
    Signed-off-by: Zorro Liu <lyx@rock-chips.com>

--- hwcomposer.cpp
+++ hwcomposer.cpp
@@ -4657,7 +4657,7 @@ static int hwc_set(hwc_composer_device_1_t *dev, size_t num_displays,
   if(atoi(value) > 0){
       property_get("debug.mode", value, "0");
       gCurrentEpdMode = atoi(value);
-      gResetEpdMode = atoi(value);
+      //gResetEpdMode = atoi(value);
       if(gCurrentEpdMode == EPD_A2){
           Region screenRegion(Rect(0, 0, ebc_buf_info.width, ebc_buf_info.height));
           currentA2Region.orSelf(screenRegion);
